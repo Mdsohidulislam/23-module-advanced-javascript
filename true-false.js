673277c4e52f4012c7f185d0e2e10c27a540f77a
sohidull <mdsohidulislam@gmail.com>
2020-08-27 09:04:36 +0600

    2 added

---
+++ true-false.js
@@ -0,0 +1,41 @@
+var age=0;///value jodi purno songkha hoi tahole true ar value jodi 0 hoi tahole false
+// if(age){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false');
+// }
+
+// var name='';
+// if(name){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false');
+// }
+
+// var name='md sohidul islam';
+// if(name){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false');
+// }
+
+
+// let name=null;
+// if(name){
+//     console.log('condition is true');
+// }else{
+//     console.log('condition is false');
+// }
+
+// let name=undefined;
+// let name=undefined;
+// let name=NaN;
+// let name=[];
+// let name={};
+// let name;
+var name=0;
+if(name || name==0){
+    console.log('condition is true');
+}else{
+    console.log('condition is false');
+}
---
+++ undefined.js
@@ -0,0 +1,21 @@
+// let name;
+// function add(num,num2){
+//     sum=num+num2;
+// }
+// var result=add(33,2);
+// console.log(result);
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+// var result=add(33);
+// console.log(result);
+
+// let premik={name:'md sohidul islam',age:33};
+// console.log(premik.college);
+var name=undefined;
+console.log(name);
+
+// null vs undefined
+// অপরিজ্ঞাত একটি পরিবর্তনশীল যা ঘোষিত হয়েছে তবে মান নির্ধারিত হয়নি।
+// একটি অ্যাসাইনমেন্ট মান হিসাবে নাল। সুতরাং আপনি মানটিকে যে কোনও পরিবর্তনযোগ্যকে নাল নির্ধারণ করতে পারবেন যার মূলত এটি ফাঁকা S যাইহোক, আপনি যখন কোনও ভেরিয়েবলের নাল বরাদ্দ করেন, আপনি বিবরণ দিয়েছিলেন যে এই মানটি স্পষ্টতই খালি।
